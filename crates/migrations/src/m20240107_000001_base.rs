6f641e3eb0798805350f49f7dd3dfc8c9db775dc
Alex Denisova <alexadenisova@gmail.com>
2024-08-14 12:02:30 +0300

    removed ingredient_name and possible_recipes

--- crates/migrations/src/m20240107_000001_base.rs
+++ crates/migrations/src/m20240107_000001_base.rs
@@ -31,35 +31,6 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(IngredientNames::Table)
-                    .col(ColumnDef::new(IngredientNames::Id).uuid().primary_key())
-                    .col(
-                        ColumnDef::new(IngredientNames::IngredientId)
-                            .uuid()
-                            .not_null(),
-                    )
-                    .col(ColumnDef::new(IngredientNames::Name).string().not_null())
-                    .col(
-                        ColumnDef::new(IngredientNames::CreatedAt)
-                            .timestamp()
-                            .not_null()
-                            .default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)),
-                    )
-                    .foreign_key(
-                        ForeignKey::create()
-                            .from_tbl(IngredientNames::Table)
-                            .from_col(IngredientNames::IngredientId)
-                            .to_tbl(Ingredients::Table)
-                            .to_col(Ingredients::Id)
-                            .on_delete(ForeignKeyAction::Cascade),
-                    )
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .create_table(
-                Table::create()
                     .table(Users::Table)
                     .col(ColumnDef::new(Users::Id).uuid().primary_key())
                     .col(ColumnDef::new(Users::Name).string().not_null())
@@ -90,7 +61,12 @@ impl MigrationTrait for Migration {
                 Table::create()
                     .table(PantryItems::Table)
                     .col(ColumnDef::new(PantryItems::Id).uuid().primary_key())
-                    .col(ColumnDef::new(PantryItems::IngredientId).uuid().not_null())
+                    .col(
+                        ColumnDef::new(PantryItems::IngredientId)
+                            .uuid()
+                            .not_null()
+                            .unique_key(),
+                    )
                     .col(ColumnDef::new(PantryItems::ExpirationDate).date())
                     .col(ColumnDef::new(PantryItems::Quantity).integer())
                     .col(ColumnDef::new(PantryItems::WeightGrams).integer())
@@ -253,9 +229,6 @@ impl MigrationTrait for Migration {
             .drop_table(Table::drop().table(PantryItems::Table).to_owned())
             .await?;
         manager
-            .drop_table(Table::drop().table(IngredientNames::Table).to_owned())
-            .await?;
-        manager
             .drop_table(Table::drop().table(Ingredients::Table).to_owned())
             .await?;
         manager
@@ -272,15 +245,6 @@ pub enum Ingredients {
     Name,
     CreatedAt,
     // InSeason,
-}
-
-#[derive(Iden)]
-pub enum IngredientNames {
-    Table,
-    Id,
-    IngredientId,
-    Name,
-    CreatedAt,
 }
 
 #[derive(Iden)]
